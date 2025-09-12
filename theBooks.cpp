874f0e28e36ab4b01263e9d52a200ed2e0d61e93
fikrte <fikrtegebretsadkan23@gmail.com>
2025-09-13 01:32:05 +0300

    InsertBook formed

--- theBooks.cpp
+++ theBooks.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 enum switch1{InsertBook=1, ViewBook, FindBook, ModifyBook};
 int main(){
-
+string title,language,author;
+int quantity, isbn, num1;
 switch1 choose;
 char symbol[100];
 
@@ -28,6 +29,22 @@ cout<<"\n1.Add new books"<<endl
       choose = static_cast<switch1>(tempoChoose);
 switch(choose){
     case InsertBook:
+      cout<<"How many books do you want to enter? ";
+	       	        cin>>num1;
+	       for(int i=0; i<num1; i++){
+		  	 cout<<"BOOK "<<i+1<<endl;
+			   cout<<"    1.Title: ";
+		     cin>> title;			    
+		  	 cout<<"    2.Author: ";
+		  	 cin>>author;
+		     cout<<"    3.ISBN: ";
+		  	 cin>>isbn;
+		     cout<<"    4.Language: ";
+		  	 cin>>language;
+		     cout<<"    5.Quantity: ";
+		     cin>> quantity;
+			  }	        
+
       break;
     case ViewBook:
       break;
